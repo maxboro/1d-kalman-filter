7b8389b7861489fd247135c1a1a23e23040ea907
Max B. <maksym_borovyk@ukr.net>
2025-07-15 11:55:13 +0300

    Add settings

--- main.cpp
+++ main.cpp
@@ -1,6 +1,15 @@
 // #include "./include/main.cpp"
 #include <iostream>
 
+// settings
+constexpr float position_estimate = 0; // What we believe about position now (starts at 0)
+constexpr float uncertainty = 1;       // How uncertain we are about that belief (starts high)
+constexpr float process_noise = 0.01;  // "Model error" – how much we think the position could change unpredictably between steps
+constexpr float measurement_noise = 0.5; // How noisy we believe our sensor is
+constexpr float true_position = 5.0;
+constexpr float n_measurements = 20;
+constexpr bool verbose = true;
+
 
 int main(){
     std::cout << "Test" << std::endl;
